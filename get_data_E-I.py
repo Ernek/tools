16f7a6f4bb0feba3a16990c19245dba44980f309
Ern3k <ebaezcheco@gmail.com>
2022-04-13 16:30:48 -0700

    include option to get variable number of states

--- get_data_E-I.py
+++ get_data_E-I.py
@@ -16,7 +16,7 @@ def read_energy_file(energy_file):
 
     return df_ener
 
-def read_osct_file(osct_file, energy_file, nstates):
+def read_osct_file(osct_file, energy_file, nstates,nstate_to_keep):
     df_osct = pd.read_csv(osct_file, sep='\s+', header=None)
     df_osct.drop(df_osct.columns[[0,1,2,3,5,7]], axis=1, inplace=True)
     df_osct.columns = ['State', 'Excited State', 'Oscillator Strength']
@@ -25,17 +25,19 @@ def read_osct_file(osct_file, energy_file, nstates):
     for i in range(1,nstates+1):
         df_osct_state = df_osct[df_osct['State'] == i].reset_index(drop=True)
         df_osct_state['Exc Energy'] = df_ener['DeltaE_meV'][i:].reset_index(drop=True) 
-        #df_osct_gs2 = df_osct[df_osct['State'] == 2].reset_index(drop=True)
-        #df_osct_gs2['Exc Energy'] = df_ener['DeltaE_meV'][2:].reset_index(drop=True)
         dict_gs_total[f"{str(i)}"] = df_osct_state
 
-    df_gs_total = pd.concat(dict_gs_total).reset_index(drop=True)
-    print(df_gs_total)
+    if nstate_to_keep:
+        keys_to_check = [number for number in range(nstates - nstate_to_keep + 1, nstates + 1)]
+        # { f"{x}": dict_gs_total[f"{x}"] for x in keys_to_check} generates a dictionary only keeping the key, value pairs of in keys_to_check : last 'nstate_to_keep' states 
+        df_gs_total = pd.concat( { f"{x}": dict_gs_total[f"{x}"] for x in keys_to_check} )
+    else: 
+        df_gs_total = pd.concat(dict_gs_total).reset_index(drop=True)
     
     return df_osct, df_gs_total     
 
-def save_energ_osct_data_to_file(osct_file, energy_file, base_output_name, nstates):
-    df_osct, df_gs_total = read_osct_file(osct_file, energy_file, nstates)
+def save_energ_osct_data_to_file(osct_file, energy_file, base_output_name, nstates, nstate_to_keep):
+    df_osct, df_gs_total = read_osct_file(osct_file, energy_file, nstates, nstate_to_keep)
     df_gs_total[['State', 'Exc Energy','Oscillator Strength']].to_csv(f'data_to_plot_{base_output_name}.txt', sep=' ', header=False, index=False)
     df_gs_total[['State', 'Exc Energy','Oscillator Strength']].to_csv(f'data_to_plot_{base_output_name}.csv', sep=' ', header=False, index=False)
 
@@ -48,7 +50,8 @@ if __name__ == '__main__':
     energy_file = sys.argv[2]  # grep 'Energy (' Output from gaussian'
     base_output_name = sys.argv[3]
     nstates = int(sys.argv[4])
+    nstate_to_keep = int(sys.argv[5])
     #df_osct , df_gs_total = read_osct_file(osct_file,energy_file,2)
     #print(df_gs_total)
 
-    save_energ_osct_data_to_file(osct_file, energy_file, base_output_name, nstates)
+    save_energ_osct_data_to_file(osct_file, energy_file, base_output_name, nstates, nstate_to_keep)
